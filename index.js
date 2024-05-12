3ff8fa8e0dd5dcb24ddcd262250c4cf23c78b59b
tuoioi67 <joacofacalpun0suy@hotmail.com>
2024-05-12 17:30:57 +0700

    feat task

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function combinationSum(candidates, target) {
-  const result = [];
-  backtrack(0, [], 0);
-  return result;
-  function backtrack(start, current, sum) {
-    if (sum === target) {
-      result.push([...current]);
-      return;
-    }
-    if (sum > target || start === candidates.length) return;
-    for (let i = start; i < candidates.length; i++) {
-      current.push(candidates[i]);
-      backtrack(i, current, sum + candidates[i]);
-      current.pop();
-    }
-  }
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
